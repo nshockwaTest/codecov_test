5b1d7b499e34aaee2e98b996775844854bea3bc3
Nick <shockwawebsites@gmail.com>
2019-04-24 19:33:58 -0700

    fixed syntax error

--- src/test/scala/com/awesomeness/TestAwesome.scala
+++ src/test/scala/com/awesomeness/TestAwesome.scala
@@ -10,6 +10,6 @@ class TestAwesome extends JUnitSuite {
  @ Test def awesomeExample {
     assertEquals(1, awesome square 1)
     assertEquals(6, awesome multiple (2,3))
-    assertEquals(2 awesome divide(4,2))
+    assertEquals(2, awesome divide(4,2))
   }
 }
